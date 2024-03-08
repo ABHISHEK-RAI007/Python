ac71994d526d78b1f70497b63636f73c5f68c0e3
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-03-08 08:55:02 +0530

    questions on loop

---
+++ 02_conditionals/03.py
@@ -0,0 +1,19 @@
+score = int(input("Enter your score: "))
+if score >= 101:
+    print("Plese veirfy your again")
+    exit()
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else: 
+    grade = "F"
+
+print('Your Grade is ', grade)
+
+
